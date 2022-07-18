cb58c8ec59bbf8166e3d9e848b81bd32544af9ec
ZennoFact <zenno04.g@gmail.com>
2022-07-18 17:28:57 +0900

    1-3-3

--- src/js/list.js
+++ src/js/list.js
@@ -2,8 +2,20 @@ const userName = "Your Name";
 const likes = ["革靴", "スーツ", "懐中時計", "万年筆"];
 const foods = ["寿司", "生肉", "生魚", "焼き鳥（塩）", "焼いた羊肉", "新鮮な鹿肉"];
 const langs = ["Scala", "Elixir", "Rust"];
-// 開発者ツールから確認
-console.log(userName);
-console.log(likes);
-console.log(foods);
-console.log(langs);
+
+likes.forEach(item => {
+    console.log(item);
+});
+
+foods.forEach(food => {
+    console.log(food);
+});
+
+langs.forEach(lang => {
+    console.log(lang);
+});
+
+let like = "スーツ";
+if (likes.indexOf(like) != -1) {
+    console.log("この人は" + like + "が好きです");
+}
