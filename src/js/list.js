5c8954c30aeb3440e98ee94aaec8c46b12593383
ZennoFact <zenno04.g@gmail.com>
2022-07-18 17:24:10 +0900

    1-3-2

---
+++ src/js/list.js
@@ -0,0 +1,9 @@
+const userName = "Your Name";
+const likes = ["革靴", "スーツ", "懐中時計", "万年筆"];
+const foods = ["寿司", "生肉", "生魚", "焼き鳥（塩）", "焼いた羊肉", "新鮮な鹿肉"];
+const langs = ["Scala", "Elixir", "Rust"];
+// 開発者ツールから確認
+console.log(userName);
+console.log(likes);
+console.log(foods);
+console.log(langs);
