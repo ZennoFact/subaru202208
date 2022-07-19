f5c42c6f5f2fbf27e49adac9051a845e556c758f
ZennoFact <zenno04.g@gmail.com>
2022-07-19 13:16:10 +0900

    教材の整理

--- src/js/list.js
+++ src/js/list.js
@@ -1,7 +1,4 @@
 const userName = "Your Name";
-// const likes = ["革靴", "スーツ", "懐中時計", "万年筆"];
-// const foods = ["寿司", "生肉", "生魚", "焼き鳥（塩）", "焼いた羊肉", "新鮮な鹿肉"];
-// const langs = ["Scala", "Elixir", "Rust"];
 
 const titles = {
     likes: "好きなもの",
@@ -13,29 +10,6 @@ const data = {
     foods: ["寿司", "生肉", "生魚", "焼き鳥（塩）", "焼いた羊肉", "新鮮な鹿肉"],
     langs: ["Scala", "Elixir", "Rust"]
 };
-
-// まずは1か所リストに like-listクラスを持たせて確認のしてから使おう
-// let list = document.querySelector("div.like-list ul");
-// likes.forEach(item => {
-//     const node = document.createElement("li");
-//     node.innerHTML = item;
-//     list.appendChild(node);
-// });
-
-// 全データを自動でUIパーツに反映
-// Object.keys(data).forEach(key => {
-//     const list = document.querySelector(`div.container.${key} ul`);
-    
-//     data[key].forEach(item => {
-//         const node = document.createElement("li");
-//         node.innerHTML = item;
-//         list.appendChild(node);
-//     });
-//     // リンク先も，自動で設定したいので，ファイル名含め，変更する
-//     const anchor = document.querySelector(`div.container.${key} a`);
-//     anchor.href = `./${key}.html`;
-// });
-    
 
 const main = document.querySelector(".main");
 // templateタグが利用可能かによって分けます
@@ -84,12 +58,3 @@ if ('content' in document.createElement('template')) {
         main.appendChild(div);
     });
 }
-
-
-
-
-
-// let like = "スーツ";
-// if (likes.indexOf(like) != -1) {
-//     console.log("この人は" + like + "が好きです");
-// }
